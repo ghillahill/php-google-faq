1814562eb8bb0f514469c03bc3c1a5445b8cc5e9
Alberto <thebertsinfo@gmail.com>
2020-12-16 16:24:50 +0100

    Init of Laravel Mix

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="public/app.css">
+        <title>PHP Google FAQ</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
